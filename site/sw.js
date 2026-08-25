869bf7d821a608e5aed8b8eb222cd13fe0a33111
kds1010 <32709992+kds1010@users.noreply.github.com>
2026-08-25 20:21:43 +0900

    Move deployment info to app header

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "daily-reader-v42";
-const APP_ASSETS = ["./", "./index.html", "./style.css?v=42", "./app.js?v=42", "./icons/icon.svg"];
+const CACHE_NAME = "daily-reader-v43";
+const APP_ASSETS = ["./", "./index.html", "./style.css?v=43", "./app.js?v=42", "./icons/icon.svg"];
 
 async function cacheSuccessfulResponse(request, response) {
   if (response?.ok) {
